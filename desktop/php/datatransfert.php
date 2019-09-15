a3a2e220cf4bc4864b47584a282ff0b9b8ffb2d2
JEALG <jalain.gosse@icloud.com>
2019-09-15 18:38:54 +0200

    Update pour php 7.3 + core V4

--- desktop/php/datatransfert.php
+++ desktop/php/datatransfert.php
@@ -11,6 +11,236 @@ $eqLogics = eqLogic::byType('datatransfert');
 </script>
 
 <div class="row row-overflow">
+	<div class="col-xs-12 eqLogicThumbnailDisplay">
+		<legend><i class="icon loisir-two28"></i> {{Gestion}}</legend>
+		<div class="eqLogicThumbnailContainer">
+			<div class="cursor eqLogicAction logoPrimary" data-action="add" >
+				<i class="fas fa-plus-circle"></i>
+				<br/>
+				<span>{{Ajouter}}</span>
+			</div>
+		</div>
+		<legend><i class="fas fa-table"></i> {{Mes Equipements Datatransfert}}</legend>
+		<input class="form-control" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
+		<div class="eqLogicThumbnailContainer">
+			<?php
+			foreach ($eqLogics as $eqLogic) {
+				$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
+				echo '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogic->getId() . '" >';
+				echo '<img src="plugins/datatransfert/doc/images/datatransfert_icon.png"  />';
+				echo '<br>';
+				echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
+				echo '</div>';
+			}
+			?>
+		</div>
+	</div>
+    <div class="col-xs-12 eqLogic" style="display: none;">
+		<div class="input-group pull-right" style="display:inline-flex">
+			<span class="input-group-btn">
+				<a class="btn btn-default btn-sm eqLogicAction" data-action="configure"><i class="fas fa-cogs"></i> {{Configuration avancée}}</a><a class="btn btn-default btn-sm eqLogicAction" data-action="copy"><i class="fas fa-copy"></i> {{Dupliquer}}</a><a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}</a><a class="btn btn-danger btn-sm eqLogicAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
+			</span>
+		</div>
+        <ul class="nav nav-tabs" role="tablist">
+			<li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fas fa-arrow-circle-left"></i></a></li>
+			<li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer-alt"></i> {{Equipement}}</a></li>
+            <li role="presentation"><a href="#eqparatab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fas fa-wrench"></i> {{Paramètres}}</a></li>
+			<li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fas fa-list-alt"></i> {{Commandes}}</a></li>
+		</ul>
+        <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+            <div role="tabpanel" class="tab-pane active" id="eqlogictab">
+				<br/>
+				<form class="form-horizontal">
+					<fieldset>
+						<div class="form-group">
+							<label class="col-sm-2 control-label">{{Nom de l'équipement virtuel}}</label>
+							<div class="col-sm-3">
+								<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+								<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement virtuel}}"/>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-sm-2 control-label" >{{Objet parent}}</label>
+							<div class="col-sm-3">
+								<select class="form-control eqLogicAttr" data-l1key="object_id">
+									<option value="">{{Aucun}}</option>
+									<?php
+									foreach (jeeObject::all() as $object) {
+										echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+									}
+									?>
+								</select>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-sm-2 control-label">{{Catégorie}}</label>
+							<div class="col-sm-8">
+								<?php
+								foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
+									echo '<label class="checkbox-inline">';
+									echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
+									echo '</label>';
+								}
+								?>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-sm-2 control-label"></label>
+							<div class="col-sm-9">
+								<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+								<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+							</div>
+						</div>
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label">{{Type}}</label>
+                            <div class="col-sm-3">
+                                <select class="form-control eqLogicAttr" data-l1key="configuration" data-l2key="protocol">
+                                    <option disabled selected value>-- {{Choisir un type}} --</option>
+                                    <?php
+                                    foreach (datatransfert::supportedProtocol() as $protocol) {
+                                        echo '<option value="' . $protocol . '">' . $protocol . '</option>';
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+            <div role="tabpanel" class="tab-pane" id="eqparatab">
+                <br/>
+                <form class="form-horizontal">
+                    <fieldset>
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label">{{Type}}</label>
+                            <div class="col-sm-3">
+                                <select class="form-control eqLogicAttr" data-l1key="configuration" data-l2key="protocol">
+                                    <option disabled selected value>-- {{Choisir un type}} --</option>
+                                    <?php
+                                    foreach (datatransfert::supportedProtocol() as $protocol) {
+                                        echo '<option value="' . $protocol . '">' . $protocol . '</option>';
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                        
+                        
+	<div class="form-group cancontrol">
+		<label class="col-sm-6 control-label help" data-help="{{Utile pour savoir quelle antenne contrôlera l'équipement. Choisir Tous aura la conséquence de déclencher potentiellement l'action autant de fois qu'il y a d'antennes}}">{{Antenne d'émission}}</label>
+		<div class="col-sm-4">
+			<select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="antenna">
+				<?php
+				if (config::byKey('noLocal', 'blea', 0) == 0){
+					echo '<option value="local">{{Local}}</option>';
+				}
+				try{
+					$hasblea = plugin::byId('blea');
+					} catch (Exception $e) {
+				}
+				if ($hasblea != '' && $hasblea->isActive()){
+					$remotes = blea_remote::all();
+					foreach ($remotes as $remote) {
+						echo '<option value="' . $remote->getId() . '">{{Remote : ' . $remote->getRemoteName() .'}}</option>';
+					}
+				}
+				?>
+				<option value="all">{{Tous}}</option>
+			</select>
+		</div>
+	</div>
+             <div class="row">
+      <div class="col-sm-6">
+        <form class="form-horizontal">
+          <fieldset>
+	<legend><i class="fab fa-bluetooth"></i>  {{Antennes}}</legend>
+	
+		</fieldset>
+    </form>
+  </div>
+
+</div>
+                                 
+                        
+                        
+                        
+                        
+                        
+                        
+                        <div class="form-group">
+                            <div id="div_protocolParameters"></div>
+                        </div>
+                    </fieldset>
+                </form>
+				
+			</div>
+            <div role="tabpanel" class="tab-pane" id="commandtab">
+				<a class="btn btn-default btn-sm pull-right cmdAction" data-action="add" style="margin-top:5px;"><i class="fas fa-plus-circle"></i> {{Ajouter une commande Data transfert}}</a>
+				<br/><br/>
+				<table id="table_cmd" class="table table-bordered table-condensed">
+					<thead>
+						<tr>
+							<th style="width: 330px;">{{Nom}}</th>
+							<th style="width: 550px;">{{source}}</th>
+                            <th style="width: 550px;">{{cible}}</th>
+                            <th style="width: 210px;">{{filtre}}</th>
+							<th>{{Valeur}}</th>
+							<th style="width: 50px;"></th>
+						</tr>
+					</thead>
+					<tbody>
+						
+					</tbody>
+				</table>
+				
+			</div>
+            
+    </div>
+    
+    
+
+    
+    
+    
+    
+    
+    
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="row row-overflow">
     <div class="col-lg-2 col-md-3 col-sm-4">
         <div class="bs-sidebar">
             <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
@@ -67,7 +297,7 @@ foreach ($eqLogics as $eqLogic) {
                             <select class="eqLogicAttr form-control" data-l1key="object_id">
                                 <option value="">{{Aucun}}</option>
                                 <?php
-foreach (object::all() as $object) {
+foreach (jeeobject::all() as $object) {
 	echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
 }
 ?>
